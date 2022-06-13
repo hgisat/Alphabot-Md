157315e5232a3decfd521bc68f9996bba4493f08
GanzzLyyn <107197636+GanzzLyyn@users.noreply.github.com>
2022-06-13 22:41:12 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -12,7 +12,7 @@ global.APIKeys = {
 }
 
 // setting 
-global.autoread = false // auto read pesan / message
+global.autoread = true // auto read pesan / message
 //global.autorecording = true //status auto merekam ( auto record )
 //global.autoketik = false //status auto mengetik (auto typing)
 //global.available = false //status online (online)
@@ -42,16 +42,16 @@ global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutp
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
 global.mess = {
-    sukses: '🤗Done, Oke Desu~',
-    admin: 'Perintah Ini Hanya Bisa Digunakan Oleh Admin !',
-    botAdmin: 'Perintah Ini Hanya Bisa Digunakan Ketika Bot Menjadi Admin !',
-    owner: 'Perintah Ini Hanya Bisa Digunakan Oleh Owner !',
-    group: 'Perintah Ini Hanya Bisa Digunakan Di Group !',
-    private: 'Perintah Ini Hanya Bisa Digunakan Di Private !',
-	bot: 'Fitur Khusus Pengguna Nomor Bot',
+    sukses: 'Done Bre',
+    admin: 'Admin Aja Kaga Kok Lu Ngatur',
+    botAdmin: 'Bot Harus Jadi Admin!!!!',
+    owner: 'Yakin Dek? Gunain Pitur ini',
+    group: 'Khusus Grup Dekk',
+    private: 'lewat japri dekk',
+	bot: 'Khusus Bot Dekk',
     errtoimg: 'Maaf Saat Ini Belum Support Sticker Gif !',
-    wait: '⏳ Sedang Di Proses',
-	lockCmd: 'Fitur Tidak Diaktifkan Oleh Owner!',
+    wait: 'Masih Proses Sabar Aja🗿',
+	lockCmd: 'Owner Gw Aja Bukan',
 	example1: 'Selamat Datang @user Di Group @subject Jangan Lupa Baca Rules @desc\n\nNote :\n1. @user (Mention User Join)\n2. @bio (Get Bio User Join)\n3. @tanggal (Date Now)\n4. @desc (Get Description Group)\n5. @subject (Group Name)'
 }
 
